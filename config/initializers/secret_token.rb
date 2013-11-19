d05b6a42b7b6632c5d2431e150954cc6f9de4338
Brett Wetzell <13.vvetz@gmail.com>
2013-11-18 22:30:10 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyDashboard::Application.config.secret_token = '1f75d28783fecb9311b3f74ee0d3075a864cf48c842ae66a3ac333b40f5142ad51cadf673af4ce6eb9f7f60bc8663fe446bb917d6f17317aa1f2f68241f42a23'
